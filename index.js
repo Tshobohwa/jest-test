0cd515f0de3832c984620e4f15e6c3091ba5f9cf
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-03-11 03:10:05 +0200

    Add error on string length more than 10 or equal to 0

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 "use strict";
 const stringLength = (string) => {
-  return string.split("").length;
+  const length = string.length;
+  if (length === 0) {
+    throw Error("The string should have at least one character");
+  } else if (length > 10) {
+    throw Error("The string should not have more than 10 characters");
+  } else return length;
 };
-console.log(stringLength("Hello"));
+console.log(stringLength(""));
 
 module.exports = stringLength;
