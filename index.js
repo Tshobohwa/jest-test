2d03815aa203b207b14117452ac8e4de875da1f6
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-03-11 04:10:24 +0200

    Add Error throwing tests

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-"use strict";
 const stringLength = (string) => {
   const length = string.length;
   if (length === 0) {
-    throw Error("The string should have at least one character");
+    throw new Error("The string should have at least one character");
   } else if (length > 10) {
-    throw Error("The string should not have more than 10 characters");
-  } else return length;
+    throw new Error("The string should not have more than 10 characters");
+  } else {
+    return length;
+  }
 };
-console.log(stringLength(""));
-
-module.exports = stringLength;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,17 @@
 const stringLength = require("./index.js");
 
-test("expect string length of hello to equal 4", () => {
-  expect(stringLength("Hello")).toBe(5);
+test("expect string length of hello to be 5", () => {
+  expect(stringLength("hello")).toBe(5);
+});
+
+test("expect string length of an empty string to throw an error", () => {
+  expect(() => {
+    stringLength("");
+  }).toThrow(Error);
+});
+
+test("expect string length of more than 10 to throw an error", () => {
+  expect(() => {
+    stringLength("12345678901");
+  }).toThrow(Error);
 });
